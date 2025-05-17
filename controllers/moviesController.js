bf5fc2ad5aa3f3f412f49d0948e7f2e1f492fcf0
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-17 14:52:18 +0200

    Add detail page, edit & delete functions

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -25,3 +25,32 @@ exports.addMovie = (req, res) => {
     saveMovies(movies);
     res.redirect('/');
 };
+exports.movieDetails = (req, res) => {
+    const movie = movies[req.params.id];
+    if (!movie) return res.status(404).send('Film nie znaleziony.');
+    res.render('movies/details', { movie, id: req.params.id });
+};
+
+// Форма редактирования
+exports.editMovieForm = (req, res) => {
+    const movie = movies[req.params.id];
+    if (!movie) return res.status(404).send('Film nie znaleziony.');
+    res.render('movies/edit', { movie, id: req.params.id });
+};
+
+// Обновить фильм
+exports.updateMovie = (req, res) => {
+    const { title, director, rating, status, review } = req.body;
+    const id = req.params.id;
+    movies[id] = { title, director, rating, status, review };
+    saveMovies(movies);
+    res.redirect('/');
+};
+
+// Удалить фильм
+exports.deleteMovie = (req, res) => {
+    const id = req.params.id;
+    movies.splice(id, 1);
+    saveMovies(movies);
+    res.redirect('/');
+};
