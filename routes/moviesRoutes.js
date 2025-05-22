63cfd114da581714df018a6634dba1c42c86bdc9
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-22 16:16:59 +0200

    Update page style with all views ejs files

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -2,21 +2,16 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/moviesController');
 
-// Главная
 router.get('/', moviesController.index);
 
-// Добавить фильм
 router.get('/add', moviesController.addMovieForm);
 router.post('/add', moviesController.addMovie);
 
-// Детали фильма
 router.get('/details/:id', moviesController.movieDetails);
 
-// Редактировать
 router.get('/edit/:id', moviesController.editMovieForm);
 router.post('/edit/:id', moviesController.updateMovie);
 
-// Удалить
 router.post('/delete/:id', moviesController.deleteMovie);
 
 module.exports = router;
